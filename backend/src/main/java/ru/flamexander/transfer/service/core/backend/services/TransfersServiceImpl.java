b356f5ff376dd8f436cfaad6500a41062c134766
Alexander Fisunov <fisunov.a.v@gmail.com>
2024-08-12 22:00:36 +0300

    lesson5

--- backend/src/main/java/ru/flamexander/transfer/service/core/backend/services/TransfersServiceImpl.java
+++ backend/src/main/java/ru/flamexander/transfer/service/core/backend/services/TransfersServiceImpl.java
@@ -4,9 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.flamexander.transfer.service.core.api.dtos.ExecuteTransferDtoRequest;
+import ru.flamexander.transfer.service.core.backend.configurations.AppProperties;
+import ru.flamexander.transfer.service.core.backend.dtos.TransferDto;
 import ru.flamexander.transfer.service.core.backend.entities.Account;
+import ru.flamexander.transfer.service.core.backend.entities.Transfer;
 import ru.flamexander.transfer.service.core.backend.errors.AppLogicException;
+import ru.flamexander.transfer.service.core.backend.repositories.TransfersRepository;
 import ru.flamexander.transfer.service.core.backend.validators.ExecuteTransferValidator;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +20,8 @@ public class TransfersServiceImpl implements TransfersService {
     private final AccountsService accountsService;
     private final ExecuteTransferValidator executeTransferValidator;
     private final ClientsInfoService clientsInfoService;
-    private final LimitService limitService;
+    private final AppProperties appProperties;
+    private final TransfersRepository transfersRepository;
 
     @Transactional
     @Override
@@ -25,19 +32,34 @@ public class TransfersServiceImpl implements TransfersService {
         if (clientsInfoService.isClientBlocker(executeTransferDtoRequest.getReceiverId())) {
             throw new AppLogicException("RECEIVER_IS_BLOCKED", "Невозможно выполнить перевод заблокированному клиенту с id = " + executeTransferDtoRequest.getReceiverId());
         }
-        if (!limitService.getLimit(clientId, executeTransferDtoRequest.getTransferSum())){
-            throw new AppLogicException("NO_HAS_SENDER_LIMIT", "У клиента id = " + clientId + " исчерпан лимит переводов. Перевод суммы " + executeTransferDtoRequest.getTransferSum()+ " невозможен");
-        }
+//        if (appProperties.getBlockedAccountsMasks().stream().anyMatch(
+//                new Predicate<String>() {
+//                    @Override
+//                    public boolean test(String s) {
+//                        return false;
+//                    }
+//                }
+//        ))
 
-        try {
-            Account senderAccount = accountsService.findByClientIdAndAccountNumber(clientId, executeTransferDtoRequest.getSenderAccountNumber()).orElseThrow(() -> new AppLogicException("TRANSFER_SOURCE_ACCOUNT_NOT_FOUND", "Перевод невозможен поскольку не существует счет отправителя"));
-            Account receiverAccount = accountsService.findByClientIdAndAccountNumber(executeTransferDtoRequest.getReceiverId(), executeTransferDtoRequest.getReceiverAccountNumber()).orElseThrow(() -> new AppLogicException("TRANSFER_TARGET_ACCOUNT_NOT_FOUND", "Перевод невозможен поскольку не существует счет получателяч"));
+        Account senderAccount = accountsService.findByClientIdAndAccountNumber(clientId, executeTransferDtoRequest.getSenderAccountNumber()).orElseThrow(() -> new AppLogicException("TRANSFER_SOURCE_ACCOUNT_NOT_FOUND", "Перевод невозможен поскольку не существует счет отправителя"));
+        Account receiverAccount = accountsService.findByClientIdAndAccountNumber(executeTransferDtoRequest.getReceiverId(), executeTransferDtoRequest.getReceiverAccountNumber()).orElseThrow(() -> new AppLogicException("TRANSFER_TARGET_ACCOUNT_NOT_FOUND", "Перевод невозможен поскольку не существует счет получателяч"));
 
-            senderAccount.setBalance(senderAccount.getBalance().subtract(executeTransferDtoRequest.getTransferSum()));
-            receiverAccount.setBalance(receiverAccount.getBalance().add(executeTransferDtoRequest.getTransferSum()));
-        } catch (AppLogicException exception){
-            limitService.rollbackLimit(clientId, executeTransferDtoRequest.getTransferSum());
-            throw exception;
-        }
+        senderAccount.setBalance(senderAccount.getBalance().subtract(executeTransferDtoRequest.getTransferSum()));
+        receiverAccount.setBalance(receiverAccount.getBalance().add(executeTransferDtoRequest.getTransferSum()));
+
+        Transfer transfer = new Transfer(
+                clientId,
+                executeTransferDtoRequest.getReceiverId(),
+                executeTransferDtoRequest.getSenderAccountNumber(),
+                executeTransferDtoRequest.getReceiverAccountNumber(),
+                executeTransferDtoRequest.getTransferSum(),
+                "SUCCESS"
+        );
+        transfersRepository.save(transfer);
+    }
+
+    @Override
+    public List<TransferDto> getTransfersHistory(Long clientId) {
+        return transfersRepository.findAllClientTransfers(clientId);
     }
 }
