51d13824bb06775d4484a4d02b259e6953829730
Hleb <gleb.qb@gmail.com>
2024-08-21 21:24:48 +0200

    added limit

---
+++ limit-service/src/main/java/ru/bublinoid/limitservice/LimitServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.bublinoid.limitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class LimitServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LimitServiceApplication.class, args);
+    }
+}
